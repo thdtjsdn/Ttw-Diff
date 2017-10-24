adb0cfc59b7bf11afdc79d9ff7460746e7611a5a
thdtjsdn <thdtjsdn@gmail.com>
2017-10-24 21:18:07 +0900

    U - API 추가 - 파일 직접 로딩 후 문자열 삽입 / 문자열 삽입 API 분리

--- Ttw-Diff.js
+++ Ttw-Diff.js
@@ -4,11 +4,16 @@ var fileNm = "Ttw-Diff.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
-var r0 = global.Lib.Ttw.diff.extract__Identifier( "./resource/pattern1-a.html", "./resource/pattern1-b.html", "((", "))" );
-var r1 = global.Lib.Ttw.diff.extract__Identifier( "./resource/pattern2-a.html", "./resource/pattern2-b.html", "((", "))" );
+
+var r0 = global.differ.extract__Identifier__FilePath( "./resource/pattern1-a.html", "./resource/pattern1-b.html", "((", "))" );
+var r1 = global.differ.extract__Identifier__FilePath( "./resource/pattern2-a.html", "./resource/pattern2-b.html", "((", "))" );
+var r2 = global.differ.extract__Identifier( SUtilFsReadStream.getFile( "./resource/pattern1-a.html" ).toString(), SUtilFsReadStream.getFile( "./resource/pattern1-b.html" ).toString(), "((", "))" );
+var r3 = global.differ.extract__Identifier( SUtilFsReadStream.getFile( "./resource/pattern2-a.html" ).toString(), SUtilFsReadStream.getFile( "./resource/pattern2-b.html" ).toString(), "((", "))" );
 
 console.log( r0 );
 console.log( r1 );
+console.log( r2 );
+console.log( r3 );
 
 //----------------------------------------------------------------------------------------------------;
 if( console ) console.log( "[ E ] - " + fileNm + "----------" );
