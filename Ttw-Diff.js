35c85cffd527289cd34fa335ef96399d8ee43849
thdtjsdn <thdtjsdn@gmail.com>
2017-11-23 12:09:52 +0900

    N, U - NodeJS 9.*.* NAPI 대응

--- Ttw-Diff.js
+++ Ttw-Diff.js
@@ -4,6 +4,7 @@ var fileNm = "Ttw-Diff.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
+/*/
 var r0 = global.differ.extract__Identifier__FilePath( "./resource/pattern1-a.html", "./resource/pattern1-b.html", "((", "))" );
 var r1 = global.differ.extract__Identifier__FilePath( "./resource/pattern2-a.html", "./resource/pattern2-b.html", "((", "))" );
 var r2 = global.differ.extract__Identifier( SUtilFsReadStream.getFile( "./resource/pattern1-a.html" ).toString(), SUtilFsReadStream.getFile( "./resource/pattern1-b.html" ).toString(), "((", "))" );
@@ -13,6 +14,12 @@ console.log( r0 );
 console.log( r1 );
 console.log( r2 );
 console.log( r3 );
+//*/
+
+var s0 = SUtilFsReadStream.getFile( "./kornet_data.txt" ).toString( "UTF8" ).trim();
+var s1 = SUtilFsReadStream.getFile( "./8801042559783.txt" ).toString( "UTF8" ).trim();
+var r4 = global.differ.extract__Identifier( s0, s1, "((", "))" );
+console.log( r4 );
 
 //----------------------------------------------------------------------------------------------------;
 if( console ) console.log( "[ E ] - " + fileNm + "----------" );
